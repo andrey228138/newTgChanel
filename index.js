ad32a97c03d5dad1c10934286f328534bfc3c4b0
andrey <tvrgb3677@gmail.com>
2025-02-25 23:57:58 +0800

    update

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded',()=>{
     if (window.innerWidth <= 1280) {
         new Swiper('.swiper-container',{
+            spaceBetween: 20,
             loop: true,
             navigation:{
                 nextEl:'.next',

cd1dbdbb00a73a5707b0a119ac0f119c70f84d2e
andrey <tvrgb3677@gmail.com>
2025-02-25 23:55:32 +0800

    update

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
 document.addEventListener('DOMContentLoaded',()=>{
-    new Swiper('.swiper-container',{
-        loop: true,
-        navigation:{
-            nextEl:'.next',
-            prevEl:'.prev'
-        },
-        pagination:{
-            el:'.swiper-pagination',
-            clickable:true,
-            dynamicBullets:true
-        },
-        scrollbar:{
-            el:'.swiper-scrollbar',
-            draggable:true,
+    if (window.innerWidth <= 1280) {
+        new Swiper('.swiper-container',{
+            loop: true,
+            navigation:{
+                nextEl:'.next',
+                prevEl:'.prev'
+            },
+            pagination:{
+                el:'.swiper-pagination',
+                clickable:true,
+                dynamicBullets:true
+            },
+            scrollbar:{
+                el:'.swiper-scrollbar',
+                draggable:true,
+    
+            }
+        })
+    }
 
-        }
-    })
 
     
 })
